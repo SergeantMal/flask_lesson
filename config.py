80a975db99bb759dda594b540b166d8c841ec4bd
SergeantMal <dersergeant@gmail.com>
2025-04-09 18:38:56 +0300

    Удален config.py из репозитория

--- config.py
+++
@@ -1,8 +0,0 @@
-DB_PARAMS = {
-    "dbname": "site_sergeant",
-    "user": "sergeant",
-    "password": "Ewxfjhyc1981!",
-    "host": "185.18.54.46",
-    "port": "5432",
-    'client_encoding': 'UTF8'
-}
